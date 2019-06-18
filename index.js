1436b6f339e507b237591c185ccfc9cc61cdabf8
Marek Trunkat <marek@trunkat.eu>
2019-06-18 09:29:39 +0200

    Updating config

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 module.exports = {
-    "extends": "airbnb-base",
+    "extends": ["airbnb-base"],
     "plugins": [
         "import",
         "promise"
     ],
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "globals": {
+        "Npm": true
+    },
     "rules": {
-        "indent": ["error", 4],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "no-underscore-dangle": [2, {
-            "allow": ["_id"],
-            "allowAfterThis": true
+          "allow": ["_id", "_outputSeqNo", "_queueOrderNo", "_skipOutput", "_retryCount", "_crashesCount", "_pageFailed", "_pageCrashed"],
+          "allowAfterThis": true
         }],
-        "no-use-before-define": 0,
         "no-param-reassign": 0,
         "consistent-return": 0,
         "array-callback-return": 0,
-        "object-curly-newline": 0,
-        "arrow-body-style": 0,
         "no-plusplus": 0,
-        "strict": ["error", "global"],
         "max-len": ["error", 150],
-        "no-undef": 0,
         "func-names": 0,
+        "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
         "import/prefer-default-export": 0,
-        "import/no-absolute-path": 0,
-        "import/no-extraneous-dependencies": ["error", { "devDependencies": ["**/test/**/*.js"] }],
+        "import/no-unresolved": 0,
+        "class-methods-use-this": 0,
+        "strict": 0,
+        "object-curly-newline": 0,
         "no-await-in-loop": 0,
-        "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"]
+        "arrow-body-style": 0,
+        "import/no-named-as-default": 0
     }
 };
-
