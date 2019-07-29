367bc357cdffa75ff8b534dc427496a46a94fa09
Ondra Urban <ondra@apify.com>
2019-07-29 17:36:02 +0200

    Update eslint config

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 module.exports = {
     "extends": ["airbnb-base"],
     "plugins": [
-        "import",
-        "promise"
+        "import"
     ],
-    "parserOptions": {
-        "ecmaVersion": 8,
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "globals": {
-        "Npm": true
+    "env": {
+        "node": true,
+        "browser": true
     },
     "rules": {
         "indent": ["error", 4, { "SwitchCase": 1 }],
         "no-underscore-dangle": [2, {
-          "allow": ["_id", "_outputSeqNo", "_queueOrderNo", "_skipOutput", "_retryCount", "_crashesCount", "_pageFailed", "_pageCrashed"],
-          "allowAfterThis": true
+            "allow": ["_id", "_outputSeqNo", "_queueOrderNo", "_skipOutput", "_retryCount", "_crashesCount", "_pageFailed", "_pageCrashed"],
+            "allowAfterThis": true
         }],
+        "no-use-before-define": 0,
         "no-param-reassign": 0,
         "consistent-return": 0,
         "array-callback-return": 0,
@@ -33,6 +28,7 @@ module.exports = {
         "object-curly-newline": 0,
         "no-await-in-loop": 0,
         "arrow-body-style": 0,
-        "import/no-named-as-default": 0
+        "import/no-named-as-default": 0,
+        "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"]
     }
 };
