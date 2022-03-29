320483ae6d80de85a0fa41068bdeaa3717e13c94
František Nesveda <frantisek@apify.com>
2022-03-29 10:28:54 +0200

    feat: add `reportUnusedDisableDirectives` to the config

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": 2021,
     },
+    "reportUnusedDisableDirectives": true,
     "rules": {
         "indent": ["error", 4, { "SwitchCase": 1 }],
         "no-underscore-dangle": [2, {
