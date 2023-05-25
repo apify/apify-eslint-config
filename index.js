d3fbdc3a6335f40879567db96b29650df916dc90
Ondra Urban <23726914+mnmkng@users.noreply.github.com>
2023-05-25 16:20:30 +0200

    fix: bump parser version to allow top level await (#11)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
         "browser": true
     },
     "parserOptions": {
-        "ecmaVersion": 2021,
+        "ecmaVersion": 2022,
     },
     "reportUnusedDisableDirectives": true,
     "rules": {
