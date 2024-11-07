e09b4e7e5da78e7c92d7a645e6a28d0e4326e5c8
Jiří Moravčík <jiri.moravcik@gmail.com>
2024-11-07 16:24:36 +0100

    feat!: flat config + v9 support, rule unification, add TS config (#14)
    Co-authored-by: František Nesveda <fnesveda@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# Usage
+# apify-eslint-config
 
-Install:
+This repository contains a shared eslint config used across Apify. It contains both JS and TS configs.
+
+## How to add to your project
+
+First install the packages as development dependencies:
 
 ```bash
-npm install --save-dev @apify/eslint-config eslint
+npm install --save-dev @apify/eslint-config eslint typescript-eslint
 ```
 
-Add `.eslintrc` file:
+Add `eslint.config.js` file, here's an example configuration for a TypeScript project using ESM:
 
-```json
-{
-  "extends": "@apify"
-}
+```js
+import apify from '@apify/eslint-config';
+
+export default [
+    ...apify,
+    {
+        languageOptions: {
+            sourceType: 'module',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+];
+
 ```
--- index.js
+++ index.js
@@ -1,66 +1,186 @@
-module.exports = {
-    "extends": ["airbnb-base"],
-    "plugins": [
-        "import"
-    ],
-    "env": {
-        "node": true,
-        "browser": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 2022,
-    },
-    "reportUnusedDisableDirectives": true,
-    "rules": {
-        "indent": ["error", 4, { "SwitchCase": 1 }],
-        "no-underscore-dangle": [2, {
-            "allow": ["_id", "_outputSeqNo", "_queueOrderNo", "_skipOutput", "_retryCount", "_crashesCount", "_pageFailed", "_pageCrashed"],
-            "allowAfterThis": true
-        }],
-        "no-use-before-define": 0,
-        "no-param-reassign": 0,
-        "consistent-return": 0,
-        "array-callback-return": 0,
-        "no-plusplus": 0,
-        "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-        "func-names": 0,
-        "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
-        "import/prefer-default-export": 0,
-        "import/no-unresolved": 0,
-        "import/extensions": 0,
-        "import/order": ["error", {
-            "groups": ["builtin", "external", ["parent", "sibling"], "index", "object"],
-            "alphabetize": { "order": "asc", "caseInsensitive": true },
-            "newlines-between": "always",
-        }],
-        "class-methods-use-this": 0,
-        "strict": 0,
-        "object-curly-newline": 0,
-        "no-await-in-loop": 0,
-        "arrow-body-style": 0,
-        "import/no-named-as-default": 0,
-        "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
-        "no-prototype-builtins": 0,
-        "max-len": ["error", {
-            "code": 160,
-            "ignoreUrls": true
-        }],
-        "no-continue": "off",
-        "function-paren-newline": 0,
-        "import/no-import-module-exports": 0,
-        "no-promise-executor-return": 0,
-        "prefer-destructuring": ["error", {
-            "VariableDeclarator": {
-                "array": false,
-                "object": true,
+const globals = require('globals');
+const js = require('@eslint/js');
+const typescriptEslint = require('typescript-eslint');
+
+const {
+    FlatCompat,
+} = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = [...compat.extends('airbnb-base'),
+    {
+        linterOptions: {
+            reportUnusedDisableDirectives: true,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
             },
-            "AssignmentExpression": {
-                "array": false,
-                "object": false,
-            }
-        }, {
-            "enforceForRenamedProperties": false,
-        }],
-        "no-return-await": 0, // So that the functions calling return show up in stack traces in case of an error
-    }
-};
+
+            ecmaVersion: 2022,
+        },
+
+        rules: {
+            indent: ['error', 4, {
+                SwitchCase: 1,
+            }],
+            // Allow underscore dangle for a few specific properties, mainly `_id`.
+            'no-underscore-dangle': [2, {
+                allow: [
+                    '_id',
+                    '_outputSeqNo',
+                    '_queueOrderNo',
+                    '_skipOutput',
+                    '_retryCount',
+                    '_crashesCount',
+                    '_pageFailed',
+                    '_pageCrashed',
+                ],
+                allowAfterThis: true,
+            }],
+            // ++ and -- are fine.
+            'no-plusplus': 'off',
+            quotes: ['error', 'single', {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            }],
+            // Disable as forcing names for function expression doesn't bring much value.
+            'func-names': 'off',
+            // Disable as this rule is overly strict and can cause breaking changes.
+            'class-methods-use-this': 'off',
+            // Forcing "use strict" is not necessary.
+            strict: 'off',
+            // We don't want to force newlines before and after curly braces.
+            'object-curly-newline': ['error', { 'consistent': true }],
+            // It's fine to use await in loops, e.g. for loop with awaits in it for sequential execution.
+            'no-await-in-loop': 'off',
+            // It's fine to use continue in loops.
+            'no-continue': 'off',
+            'arrow-body-style': 'off',
+            'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+            // Set max line length to 160 characters.
+            'max-len': ['error', {
+                code: 160,
+                ignoreUrls: true,
+                ignoreTemplateLiterals: true,
+            }],
+            // Allow less strict formatting of function parentheses.
+            'function-paren-newline': 'off',
+            // Prefer destructuring for certain cases only. Forcing destructuring for arrays can make the code less readable.
+            'prefer-destructuring': ['error', {
+                VariableDeclarator: {
+                    array: false,
+                    object: true,
+                },
+                AssignmentExpression: {
+                    array: false,
+                    object: false,
+                },
+            }, {
+                enforceForRenamedProperties: false,
+            }],
+            // So that the functions calling return show up in stack traces in case of an error.
+            'no-return-await': 'off',
+            // Do not enforce newlines between class members if they have a single line.
+            'lines-between-class-members': ['error', 'always', {
+                exceptAfterSingleLine: true,
+            }],
+
+            // Rules related to eslint-plugin-import.
+            // Force external modules to be specified in the package.json.
+            'import/no-extraneous-dependencies': ['error', {
+                devDependencies: true,
+            }],
+            // Force the use of named exports.
+            'import/no-default-export': 'error',
+            // It's ok to not have a default export and we force named exports anyway.
+            'import/prefer-default-export': 'off',
+            'import/no-unresolved': 'off',
+            'import/extensions': 'off',
+            // Force ordering of imports.
+            'import/order': ['error', {
+                groups: ['builtin', 'external', ['parent', 'sibling'], 'index', 'object'],
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+                'newlines-between': 'always',
+            }],
+            'import/no-import-module-exports': 'off',
+        },
+    },
+    // Configuration for TypeScript files only.
+    // Use the recommended TypeScript configs as a base, follow up with our overrides.
+    ...typescriptEslint.configs.recommended.map((conf) => ({
+        ...conf,
+
+        files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    })),
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+
+        plugins: {
+            '@typescript-eslint': typescriptEslint.plugin,
+        },
+
+        languageOptions: {
+            parser: typescriptEslint.parser,
+            ecmaVersion: 2022,
+        },
+
+        rules: {
+            // TypeScript doesn't support imports with `.ts` extensions, and `.js` makes the rule complain.
+            'import/extensions': 'off',
+            // Note: you must disable the base rule as it can report incorrect errors.
+            'no-shadow': 'off',
+            // This rule extends the base eslint/no-shadow rule. It adds support for TypeScript's this parameters and global augmentation, and adds options for TypeScript features.
+            '@typescript-eslint/no-shadow': ['error'],
+            // Not allowing non-null assertions is overly strict.
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            // Note: you must disable the base rule as it can report incorrect errors.
+            'no-useless-constructor': 'off',
+            // This rule extends the base eslint/no-useless-constructor rule for TypeScript.
+            '@typescript-eslint/no-useless-constructor': 'error',
+            // Note: you must disable the base rule as it can report incorrect errors.
+            'default-param-last': 'off',
+            // This rule extends the base eslint/default-param-last rule for TypeScript.
+            '@typescript-eslint/default-param-last': 'error',
+            // Force promises to be awaited, returned, voided, .then()ed or .catch()ed.
+            '@typescript-eslint/no-floating-promises': 'error',
+            // Disallow the use of promises in places where they shouldn't be used.
+            '@typescript-eslint/await-thenable': 'error',
+            // Do not misuse promises in places like if conditions, or passing them to functions that don't handle promises.
+            '@typescript-eslint/no-misused-promises': ['error', {
+                checksVoidReturn: {
+                    arguments: false,
+                    attributes: false,
+                },
+            }],
+            // Require methods and functions to be marked async if they return a Promise.
+            '@typescript-eslint/promise-function-async': 'error',
+            // Force the usage of "import type" for type imports.
+            '@typescript-eslint/consistent-type-imports': 'error',
+            // Force consistent array type definitions.
+            '@typescript-eslint/array-type': 'error',
+            // Force better grouping of overloaded signatures.
+            '@typescript-eslint/adjacent-overload-signatures': 'error',
+            // Disallow "in" operator with arrays. It is better to use "of".
+            '@typescript-eslint/no-for-in-array': 'error',
+            // Avoid using eval().
+            '@typescript-eslint/no-implied-eval': 'error',
+            // Avoid type specifications that are not necessary.
+            '@typescript-eslint/no-inferrable-types': 'error',
+            // Disallow usage of non-null assertion "!" next to an assignment or equality check.
+            '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+            // Prefer "includes()" over "indexOf() !== -1" when checking for existence.
+            '@typescript-eslint/prefer-includes': 'error',
+        },
+    },
+];
